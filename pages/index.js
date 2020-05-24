0482bc7f420efe079297e047258ddead543256be
Warayut Taekrathok <warayut2555@gmail.com>
2020-05-24 23:10:32 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ const Home = () => {
         <meta property="og:image" content="https://scontent.fbkk4-3.fna.fbcdn.net/v/t1.15752-9/98034808_613622932578761_192100065945321472_n.png?_nc_cat=109&_nc_sid=b96e70&_nc_eui2=AeF9MO5Xwerkmxirr3_n-yxoYmFaLbq_6ORiYVotur_o5AAy6GK4tzoACeF_ggzFnip__keqyG-vg3BKrgq-kaBw&_nc_ohc=0jcFsr2JuPQAX99xiXG&_nc_ht=scontent.fbkk4-3.fna&oh=6927fcb5f0333d82c2165e0d355ce531&oe=5EEF18C6&dl=1" />
         <meta property="og:url" content="https://lin.ee/b62lJ5I" />
       </Head>
-      <img src="https://scontent.fbkk4-3.fna.fbcdn.net/v/t1.15752-9/98034808_613622932578761_192100065945321472_n.png?_nc_cat=109&_nc_sid=b96e70&_nc_eui2=AeF9MO5Xwerkmxirr3_n-yxoYmFaLbq_6ORiYVotur_o5AAy6GK4tzoACeF_ggzFnip__keqyG-vg3BKrgq-kaBw&_nc_ohc=0jcFsr2JuPQAX99xiXG&_nc_ht=scontent.fbkk4-3.fna&oh=6927fcb5f0333d82c2165e0d355ce531&oe=5EEF18C6&dl=1" />
+      <img src="https://scontent.fbkk4-3.fna.fbcdn.net/v/t1.15752-9/98034808_613622932578761_192100065945321472_n.png?_nc_cat=109&_nc_sid=b96e70&_nc_eui2=AeF9MO5Xwerkmxirr3_n-yxoYmFaLbq_6ORiYVotur_o5AAy6GK4tzoACeF_ggzFnip__keqyG-vg3BKrgq-kaBw&_nc_ohc=0jcFsr2JuPQAX99xiXG&_nc_ht=scontent.fbkk4-3.fna&oh=6927fcb5f0333d82c2165e0d355ce531&oe=5EEF18C6&dl=1" alt="test" />
     </div>
   )
 }
